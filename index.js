0a77f6016ea831b5f1bbc5c303e076c11c57cecf
Leila Abdi <leila.abdi@student.moringaschool.com>
2023-12-12 21:10:13 +0300

    completed assigment

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  const square = (parameter) => {
+    return parameter * parameter;
+  };
+  
+  const add = (param1, param2) => {
+    return param1 + param2;
+  };
+  
+  console.log(divide()); 
+  console.log(square(5)); 
+  console.log(add(10, 7));
+  
